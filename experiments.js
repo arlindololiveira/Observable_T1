460eba83a90cacfe2c9e64f06259ac53331f37b3
Arlindo Oliveira <arlindo.oliveira@tecnico.ulisboa.pt>
2024-04-25 14:50:56 -0400

    None

---
+++ experiments.js
@@ -0,0 +1,7 @@
+let user2 = {     // an object
+    name: "Maria",  // by key "name" store value "John"
+    age: 40        // by key "age" store value 30
+};
+// get property values of the object:
+alert( user2.name ); // John
+alert( user2.age ); // 30
