af56da63070d6b730c0b3bdf994234fdb79e9f9b
Arlindo Oliveira <arlindo.oliveira@tecnico.ulisboa.pt>
2024-04-25 15:17:56 -0400

    Initial experiments with JS elements

--- experiments.js
+++ experiments.js
@@ -5,3 +5,14 @@ let user2 = {     // an object
 // get property values of the object:
 alert( user2.name ); // John
 alert( user2.age ); // 30
+
+message = "Bye Bye"
+alert(message)
+
+let fruits = ["Apple", "Orange", "Plum"]
+alert(fruits[0])
+
+function sum(a, b) {
+    return a + b;
+  }
+
